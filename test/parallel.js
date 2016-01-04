5d5da42ca53657c096aeee11c5e2556cbd4175ea
Devin Cooper <dcooper@outbrain.com>
2016-01-03 23:32:02 -0600

    Build: Clean up unit tests and add additional `step` unit test.

--- test/parallel.js
+++ test/parallel.js
@@ -5,24 +5,24 @@ describe('parallel', () => {
     let parallel1 = parallel(
       (fn) => {
         setTimeout(() => {
-          fn(1);
+          fn('a');
         }, 1000);
       },
       (fn) => {
         setTimeout(() => {
-          fn(2);
+          fn('b');
         }, 500);
       },
       (fn) => {
         setTimeout(() => {
-          fn(3);
+          fn('c');
         }, 1);
       }
     );
 
     parallel1.done((err, values) => {
       expect(err).to.be.null;
-      expect(values).to.deep.equal([1, 2, 3]);
+      expect(values).to.deep.equal(['a', 'b', 'c']);
       done();
     });
   });
--- test/step.js
+++ test/step.js
@@ -4,24 +4,24 @@ describe('step', () => {
   it('should create new instances', function() {
     let step1 = step(
       (next) => {
-        next(1);
+        next('a');
       },
       (next) => {
-        next(2);
+        next('b');
       },
       (next) => {
-        next(3);
+        next('c');
       }
     );
     let step2 = step(
       (next) => {
-        next(1);
+        next('a');
       },
       (next) => {
-        next(2);
+        next('b');
       },
       (next) => {
-        next(3);
+        next('c');
       }
     );
 
@@ -29,145 +29,163 @@ describe('step', () => {
     expect(step1.stack).to.have.length(3);
   });
 
-  describe('next', () => {
-    it('should execute the `next` function, with a synchronous iterator', (done) => {
-      let step1 = step(
-        (next) => {
-          next(1);
-        },
-        (next) => {
-          next(2);
-        },
-        (next) => {
-          next(3);
-        }
-      );
-      let step2 = step(
-        (next) => {
-          next(1);
-        },
-        (next) => {
-          next(2);
-        },
-        (next) => {
-          next(3);
-        }
-      );
+  it('should execute the `next` function, with a synchronous iterator', (done) => {
+    let step1 = step(
+      (next) => {
+        next('a');
+      },
+      (next) => {
+        next('b');
+      },
+      (next) => {
+        next('c');
+      }
+    );
+    let step2 = step(
+      (next) => {
+        next('a');
+      },
+      (next) => {
+        next('b');
+      },
+      (next) => {
+        next('c');
+      }
+    );
 
-      step1.done((err, values) => {
-        expect(err).to.be.null;
-        expect(values).to.deep.equal([1, 2, 3]);
-      });
-
-      setTimeout(() => {
-        step2.done((err, values) => {
-          expect(err).to.be.null;
-          expect(values).to.deep.equal([1, 2, 3]);
-          done();
-        });
-      }, 1000);
+    step1.done((err, values) => {
+      expect(err).to.be.null;
+      expect(values).to.deep.equal(['a', 'b', 'c']);
     });
 
-    it('should execute the `next` function, with an asynchronous iterator', (done) => {
-      let step1 = step(
-        (next) => {
-          setTimeout(() => {
-            next(1);
-          }, 1000);
-        },
-        (next) => {
-          setTimeout(() => {
-            next(2);
-          }, 500);
-        },
-        (next) => {
-          setTimeout(() => {
-            next(3);
-          }, 1);
-        }
-      );
-      let step2 = step(
-        (next) => {
-          setTimeout(() => {
-            next(1);
-          }, 100);
-        },
-        (next) => {
-          setTimeout(() => {
-            next(2);
-          }, 10);
-        },
-        (next) => {
-          setTimeout(() => {
-            next(3);
-          }, 1);
-        }
-      );
-
-      step1.done((err, values) => {
+    setTimeout(() => {
+      step2.done((err, values) => {
         expect(err).to.be.null;
-        expect(values).to.deep.equal([1, 2, 3]);
+        expect(values).to.deep.equal(['a', 'b', 'c']);
+        done();
       });
+    }, 1000);
+  });
 
-      setTimeout(() => {
-        step2.done((err, values) => {
-          expect(err).to.be.null;
-          expect(values).to.deep.equal([1, 2, 3]);
-          done();
-        });
-      }, 1000);
+  it('should execute the `next` function, with an asynchronous iterator', (done) => {
+    let step1 = step(
+      (next) => {
+        setTimeout(() => {
+          next('a');
+        }, 1000);
+      },
+      (next) => {
+        setTimeout(() => {
+          next('b');
+        }, 500);
+      },
+      (next) => {
+        setTimeout(() => {
+          next('c');
+        }, 1);
+      }
+    );
+    let step2 = step(
+      (next) => {
+        setTimeout(() => {
+          next('a');
+        }, 100);
+      },
+      (next) => {
+        setTimeout(() => {
+          next('b');
+        }, 10);
+      },
+      (next) => {
+        setTimeout(() => {
+          next('c');
+        }, 1);
+      }
+    );
+
+    step1.done((err, values) => {
+      expect(err).to.be.null;
+      expect(values).to.deep.equal(['a', 'b', 'c']);
     });
 
-    it('should stop subsequent execution, if a synchronous error is caught', (done) => {
-      let step1 = step(
-        (next) => {
-          setTimeout(() => {
-            next(1);
-          }, 1000);
-        },
-        (next) => {
-          foo();
-          setTimeout(() => {
-            next(2);
-          }, 500);
-        },
-        (next) => {
-          setTimeout(() => {
-            next(3);
-          }, 1);
-        }
-      );
-      let step2 = step(
-        (next) => {
-          foo();
-          setTimeout(() => {
-            next(1);
-          }, 100);
-        },
-        (next) => {
-          setTimeout(() => {
-            next(2);
-          }, 10);
-        },
-        (next) => {
-          setTimeout(() => {
-            next(3);
-          }, 1);
-        }
-      );
+    setTimeout(() => {
+      step2.done((err, values) => {
+        expect(err).to.be.null;
+        expect(values).to.deep.equal(['a', 'b', 'c']);
+        done();
+      });
+    }, 1000);
+  });
 
-      step1.done((err, values) => {
+  it('should stop subsequent execution, if a synchronous error is caught', (done) => {
+    let step1 = step(
+      (next) => {
+        setTimeout(() => {
+          next('a');
+        }, 1000);
+      },
+      (next) => {
+        foo();
+        setTimeout(() => {
+          next('b');
+        }, 500);
+      },
+      (next) => {
+        setTimeout(() => {
+          next('c');
+        }, 1);
+      }
+    );
+    let step2 = step(
+      (next) => {
+        foo();
+        setTimeout(() => {
+          next('a');
+        }, 100);
+      },
+      (next) => {
+        setTimeout(() => {
+          next('b');
+        }, 10);
+      },
+      (next) => {
+        setTimeout(() => {
+          next('c');
+        }, 1);
+      }
+    );
+
+    step1.done((err, values) => {
+      expect(typeof err.message).to.equal('string');
+      expect(values).to.be.undefined;
+    });
+
+    setTimeout(() => {
+      step2.done((err, values) => {
         expect(typeof err.message).to.equal('string');
         expect(values).to.be.undefined;
+        done();
       });
+    }, 1000);
+  });
 
-      setTimeout(() => {
-        step2.done((err, values) => {
-          expect(typeof err.message).to.equal('string');
-          expect(values).to.be.undefined;
-          done();
-        });
-      }, 1000);
-    });
+  it('should stop subsequent execution, if `next` is not executed', function() {
+    let values = [];
+    let fn = sinon.spy();
+    let step1 = step(
+      (next) => {
+        values.push('a');
+      },
+      (next) => {
+        values.push('b');
+      },
+      (next) => {
+        values.push('c');
+      }
+    );
+
+    step1.done(fn);
+    expect(fn.called).to.be.false;
+    expect(values).to.deep.equal(['a']);
   });
 });
